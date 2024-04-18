bd994f78333dc72ea21e53be5175e8d423cccae8
Sudipta Basak <sudiptab2100@gmail.com>
2024-04-18 16:15:58 +0530

    Store analyzed data

--- analyzer.js
+++ analyzer.js
@@ -34,7 +34,6 @@ async function main() {
     }
     
     hasher_js = hasher + '_js';
-    result = [];
     command = `snarkjs groth16 prove ${hasher_js}/prover_key.zkey ${hasher_js}/witness.wtns ${hasher_js}/proof.json ${hasher_js}/public.json`;
     const start = new Date();
     await runC(command, runs);
@@ -51,6 +50,40 @@ async function main() {
     console.log(`Prover Key size: ${zkey_size} KB`);
     console.log(`Witness size: ${witness_size} KB`);
     console.log(`Runtime: ${runtime} ms`);
+    
+    const jsonFilePath = `results/${hasher}.json`;
+    if (!fs.existsSync('results')) fs.mkdirSync('results');
+    if(!fs.existsSync(jsonFilePath)) {
+        const proof_generation = {
+            runs: 0,
+            runtime: 0,
+            avg_runtime: 0,
+        }
+        const jsonObject = {
+            wasm_size: 0,
+            r1cs_size: 0,
+            zkey_size: 0,
+            witness_size: 0,
+            proof_generation: proof_generation
+        };
+        fs.writeFileSync(jsonFilePath, JSON.stringify(jsonObject), 'utf8');
+    }
+    
+    const data = JSON.parse(fs.readFileSync(jsonFilePath, 'utf8'));
+    prev_runs = data.proof_generation.runs;
+    prev_runtime = data.proof_generation.runtime;
+    total_runs = prev_runs + runs;
+    total_runtime = prev_runtime + runtime;
+    avg_runtime = total_runtime / total_runs;
+    
+    data.wasm_size = wasm_size;
+    data.r1cs_size = r1cs_size;
+    data.zkey_size = zkey_size;
+    data.witness_size = witness_size;
+    data.proof_generation.runs = total_runs;
+    data.proof_generation.runtime = total_runtime;
+    data.proof_generation.avg_runtime = avg_runtime;
+    fs.writeFileSync(jsonFilePath, JSON.stringify(data, null, 4), 'utf8');
 }
 
 main();
