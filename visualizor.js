5442c65be111e13744f2f3129d2a82dcb8af35f7
Sudipta Basak <sudiptab2100@gmail.com>
2024-04-18 23:37:24 +0530

    final plot added

--- visualizor.js
+++ visualizor.js
@@ -38,15 +38,18 @@ wasm_sizes = [];
 r1cs_sizes = [];
 witness_sizes = [];
 zkey_sizes = [];
+runtimes = [];
 for(let i = 0; i < hasher_names.length; i++) {
     const data = JSON.parse(fs.readFileSync(`results/${hasher_names[i]}.json`, 'utf8'));
     wasm_sizes.push(data.wasm_size);
     r1cs_sizes.push(data.r1cs_size);
     witness_sizes.push(data.witness_size);
     zkey_sizes.push(data.zkey_size);
+    runtimes.push(data.proof_generation.avg_runtime);
 }
 
 generateBarGraph(hasher_names, wasm_sizes, 'results/plots/wasm_sizes.png', label='Wasm Size (KB)');
 generateBarGraph(hasher_names, r1cs_sizes, 'results/plots/r1cs_sizes.png', label='R1CS Size (KB)');
 generateBarGraph(hasher_names, witness_sizes, 'results/plots/witness_sizes.png', label='Witness Size (KB)');
 generateBarGraph(hasher_names, zkey_sizes, 'results/plots/zkey_sizes.png', label='Prover Key Size (KB)');
+generateBarGraph(hasher_names, runtimes, 'results/plots/runtimes.png', label='Average Runtime (ms)');
